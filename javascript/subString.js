d9e0646c647e61242ce68b9c777e1f184f727747
Constantino Mora <constantinomora@Constantinos-MacBook-Pro.local>
2015-06-11 23:59:45 -0500

    Using Regex to get rid of whitespaces

--- javascript/subString.js
+++ javascript/subString.js
@@ -1,7 +1,7 @@
 var string = "Languages: java, python, php, ruby, c++."
 var start = string.indexOf(':');
 var end = string.indexOf('.', start+1);
-var listStr = string.substring(start+1,end);
+var listStr = string.substring(start+2,end);
 
 console.log('original string: '+string);
 console.log('substring: '+listStr);
@@ -13,4 +13,8 @@ x.forEach(function(element, index, array){
 	array[index] = element.trim();
 })
 
-console.log('substring without spaces: ',x);
+console.log('substring x - without spaces: ',x);
+
+y = listStr.match(/\S+/g)
+
+console.log('substring y - without spaces: ',y);
