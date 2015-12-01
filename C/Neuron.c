a46ed85e9e1264d76e504401805c7b183efb7542
Constantino Mora <constantinomora@gmail.com>
2015-11-30 23:53:09 -0600

    Get inputs and weights

--- C/Neuron.c
+++ C/Neuron.c
@@ -6,13 +6,23 @@ int funcion_sigmoidal();
 
 int main(int argc, char *argv[]){
 
-	double pesos[argc-1];
+	int ctd = atof(argv[1]);
+	
+	double entradas[ctd];
+	double pesos[ctd];
+	double umbral = atof(argv[argc-1]);
 
-	for(int i=0;i<argc-1;i++){
+	for(int i=0;i<ctd;i++){
 		
-		pesos[i] = atof(argv[i+1]);
-		printf("w[%d]: %f\n", i,pesos[i]);
+		entradas[i] = atof(argv[i+2]);
+		pesos[i] = atof(argv[i+2+ctd]);
+
+		printf("entradas[%d]: %f\n", i,entradas[i]);
+		printf("pesos[%d]: %f\n", i,pesos[i]);
+
 	}
+
+	printf("umbral: %f\n", umbral);
 
 	return 0;
 }
