1a3465bcee017ea2ec35b271cfad64ea6f140fa4
Constantino Mora <constantinomora@gmail.com>
2016-02-16 02:30:42 -0600

    prototype for Shift method

--- Python/TOPTW/insertion_step.py
+++ Python/TOPTW/insertion_step.py
@@ -14,7 +14,19 @@ class insertion_step:
 		max_shift = min( closing-arrival , self.wait(opening, arrival) + self.maxShift(Locations, i+1, Locations[i+1].opening, Locations[i+1].closing, self.estimateArrival(i, i+1,times,start), times,start,end))
 
 		return max_shift	
-	
+
+	def Shift( j , i):
+		cij = EstimateArrivalFrom( IncludedLocations[i], Locations[j] )
+		wait = Locations[j].Wait
+		Tj = max_shift
+		cjk = EstimateArrivalFrom( Locations[j], IncludedLocations[i+1] )
+		cik = EstimateArrivalFrom( IncludedLocations[i],  IncludedLocations[i+1])
+
+		Shift = cij + wait + Tj + cjk - cik
+		Sum_Wait_MaxShift = IncludedLocations[i+1].Wait + IncludedLocations[i+1].MaxShift
+
+		return Shift if (Shift <= Sum_Wait_MaxShift) else Sum_Wait_MaxShift
+		
 	def update_locations(self,Locations,times,start,end):
 		len_loc = len(Locations)-1
 		for i in range(len_loc):
