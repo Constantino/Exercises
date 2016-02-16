394652cf59b68914d614704afb3bce9568f29214
Constantino Mora <constantinomora@gmail.com>
2016-02-15 22:53:37 -0600

    Added max shift method

--- Python/TOPTW/insertion_step.py
+++ Python/TOPTW/insertion_step.py
@@ -5,17 +5,22 @@ class insertion_step:
 
 	def estimateArrival(self,l_i, l_k, times):
 		return times[l_i][l_k]
+	
+	def maxShift(self, Locations, i, opening, closing, arrival, times):
+		if i == (len(Locations)-1):
+			return min( closing-arrival , self.wait(opening, arrival))
+		max_shift = min( closing-arrival , self.wait(opening, arrival)) + self.maxShift(Locations, i+1, Locations[i+1].opening, Locations[i+1].closing, self.estimateArrival(i, i+1,times), times)
 
+		return max_shift	
+	
 	def update_locations(self,Locations,times):
 		len_loc = len(Locations)-1
 		for i in range(len_loc):
 			arrival = self.estimateArrival(Locations[i].id_location,Locations[i+1].id_location,times)
 			Locations[i].wait = self.wait(Locations[i].opening, arrival)
-
-			#print "i: ",i," wait: ",Locations[i].wait
+			
+			Locations[i].max_shift = self.maxShift(Locations, i, Locations[i].opening, Locations[i].closing, arrival, times)
 			"""
-			Locations[i].MaxShift = MaxShift(Locations, i, Locations[i].opening, Locations[i].closing, arrival )
-
 			Locations[i].Shift = Shift( i, Locations.LastIndex-1 )
 			Locations[i].Ratio = Ratio( i )
 			"""
--- Python/TOPTW/main.py
+++ Python/TOPTW/main.py
@@ -6,8 +6,9 @@ def print_locations(Locations):
 		print e.id_location
 		print "-- name: ",e.name
 		print "-- wait: ",e.wait
+		
+		print "-- max_shift: ",e.max_shift
 		"""
-		print e.max_shift
 		print e.shift
 		print e.radio
 		"""
@@ -26,18 +27,21 @@ Locations[0].name = "Loc1"
 Locations[0].opening = 8
 Locations[0].closing = 19
 Locations[0].score = 4
+Locations[0].max_shift = 0
 
 Locations[1].id_location = 1		
 Locations[1].name = "Loc2"
 Locations[1].opening = 9
 Locations[1].closing = 20
 Locations[1].score = 5
+Locations[1].max_shift = 0
 
 Locations[2].id_location = 2
 Locations[2].name = "Loc3"
 Locations[2].opening = 12
 Locations[2].closing = 23
 Locations[2].score = 3
+Locations[2].max_shift = 0
 
 Locations[3].id_location = 3
 Locations[3].name = "Loc4"
@@ -45,7 +49,7 @@ Locations[3].opening = 10
 Locations[3].closing = 19
 Locations[3].score = 4
 Locations[3].wait = 0
-
+Locations[3].max_shift = 0
 
 
 times = [[0,0.5,1.3,0.3],[0.5,0,0.7,2],[1.3,0.7,0,1],[0.3,2,1,0]]
