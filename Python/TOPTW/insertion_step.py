03accc4d24f6651a8348c78b783d3f333c1a118b
Constantino Mora <constantinomora@gmail.com>
2016-02-21 21:25:11 -0600

    fixed formula errors for shift and maxshift

--- Python/TOPTW/insertion_step.py
+++ Python/TOPTW/insertion_step.py
@@ -12,9 +12,17 @@ class insertion_step:
 		return times[l_i][l_k]+start
 	
 	def maxShift(self, Locations, i, opening, closing, arrival, times,start,end):
-		if i == (len(Locations)-1):
+		
+		if i+1 == (len(Locations)-1):
 			#If we reach the last location, means it's the end of the tour
-			return 0
+			leave = Locations[i].closing if Locations[i].closing <= end else end
+			#print "leave from ",i,": ",leave
+			arr2 = times[i][i+1]
+			#print "arrival: ",arrival
+			#print "next arrival: ",arr2
+			max_shift = leave-arrival-arr2
+			#print "max_shift: ",max_shift
+			return max_shift
 
 		#We chose the maximum time allowed to stay in one place in order to not make 
 		#infeasible the staying time for the rest of locations.
@@ -32,13 +40,21 @@ class insertion_step:
 
 	def Shift( self, Locations, j , i, times, start):
 		#Get the total cost of time of including a location between location_i and location_k
-		cij = self.estimateArrival( i, j, times, start )
+		k = j+1
+		cij = times[i][j]#self.estimateArrival( i, j, times, start )
+		print "cij: ", cij
 		wait = Locations[j].wait
-		Tj = Locations[j].max_shift
-		cjk = self.estimateArrival( j, len(Locations)-1 , times, start)
-		cik = self.estimateArrival( i,  len(Locations)-1, times, start)
+		print "wait: ",wait
+		
+		cjk = times[j][k]#self.estimateArrival( j, k , times, start)
+		print "cjk: ",cjk
+		cik = times[i][k]#self.estimateArrival( i, k, times, start)
+		print "cik: ",cik
+		Tj = max(0,Locations[j].max_shift)
+		print "Tj: ",Tj
 
 		Shift = cij + wait + Tj + cjk - cik
+		print "shift: ",Shift
 		Sum_Wait_MaxShift = Locations[j].wait + Locations[j].max_shift
 
 		return Shift if (Shift <= Sum_Wait_MaxShift) else Sum_Wait_MaxShift
@@ -47,8 +63,9 @@ class insertion_step:
 		#Since location 0 is the origin and the last one the end of the tour:
 		#Iterate from the second location until the penultimate location
 		for i in range(1,len(Locations)-1):
-			Locations[i].arrival = self.estimateArrival(Locations[i].id_location,Locations[i+1].id_location,times,start)
-
+			#print "i: ",i
+			Locations[i].arrival = self.estimateArrival(Locations[i-1].id_location,Locations[i].id_location,times,start)
+			#print "Location arrival: ", Locations[i].arrival
 			Locations[i].wait = self.wait(Locations[i].opening, Locations[i].arrival)
 
 			Locations[i].max_shift = self.maxShift(Locations, 0, Locations[i].opening, Locations[i].closing, Locations[i].arrival, times,start,end)
@@ -85,8 +102,16 @@ class insertion_step:
 		Locations[k].wait = max( 0, Locations[k].wait - Locations[j].shift )
 		Locations[k].arrival = Locations[k].arrival + Locations[j].shift
 		Locations[k].shift = max(0,Locations[j].shift - Locations[k].wait)
-		print "k: ",k," shift: ",Locations[k].shift
+		print "k: ",k," loc id: ",Locations[k].id_location," shift: ",Locations[k].shift
 		#Locations[k].start = Locations[k].start + Locations[k].shift
 		Locations[k].max_shift = max(0,Locations[k].max_shift - Locations[k].shift)
 
 		return Locations
+
+	def set_at_better_position(Locations, location,start):
+		shift_list = {}
+
+		#for i in range(len(Locations)):
+
+
+
