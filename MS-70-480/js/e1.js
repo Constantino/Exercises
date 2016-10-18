c68ee49387761be90ef7670820782bb28c58a501
Constantino <constantinomora@gmail.com>
2016-10-18 17:35:28 -0500

    separating js and css

---
+++ MS-70-480/js/e1.js
@@ -0,0 +1,11 @@
+window.onload = function x(){
+
+	var element = document.createElement("article");
+	element.innerText = "My new article 1";
+
+	var outerDiv = document.getElementById("content");
+
+	outerDiv.appendChild(element);
+
+	
+}
